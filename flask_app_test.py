a3e96750ad6879926aa4d1be2426a520a855a3cc
kiu_bedukadze.lazare <bedukadze.lazare@kiu.edu.ge>
2025-04-13 23:20:41 +0400

    init

---
+++ flask_app_test.py
@@ -0,0 +1,13 @@
+import flask_app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = flask_app.app.test_client()
+
+    def test_hello(self):
+        response = self.app.get('/hello')
+        self.assertEqual(response.data.decode(), 'Hi!')
+
+if __name__ == '__main__':
+    unittest.main()
